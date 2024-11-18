48109a5d42cdce4d3a31ced30ad57f374096e392
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-11-18 18:39:05 +0100

    feat: démarrage plus robuste
    testé sur win

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-import subprocess
 import os
+import subprocess
 import platform
+import sys
 
 def run_command(command):
     """Exécute une commande shell et arrête si elle échoue."""
@@ -9,14 +10,26 @@ def run_command(command):
         print(f"Error: Command '{command}' failed!")
         exit(1)
 
-venv_dir = "venv"
+# Étape 1 : Changer le répertoire courant pour simuler une exécution depuis le répertoire parent
+script_dir = os.path.abspath(os.path.dirname(__file__))  # Chemin absolu vers le fichier actuel
+root_dir = os.path.dirname(script_dir)  # Supposons que 'routefinder' est le dossier parent
+os.chdir(root_dir)  # Change le répertoire courant pour le dossier parent
 
- # Étape 5 : Activer l'environnement virtuel
+# Étape 2 : Définir les chemins pour venv et le script à exécuter
+venv_dir = os.path.join(root_dir, "venv")
+file_to_execute = os.path.join("src", "dataPickerUI.py")  # Chemin relatif depuis le nouveau répertoire courant
+
+# Étape 3 : Activer l'environnement virtuel
+print("\nActivation de l'environnement virtuel...")
 if platform.system() == "Windows":
-    activation_command = f"{venv_dir}\\Scripts\\activate && "
+    activation_command = f"{os.path.join(venv_dir, 'Scripts', 'activate')} && "
 else:
-    activation_command = f"source {venv_dir}/bin/activate && "
+    activation_command = f"source {os.path.join(venv_dir, 'bin', 'activate')} && "
 
-# Étape 6 : Proposer d'exécuter src/buildApp.py
-file_to_execute = os.path.join("src", "dataPickerUI.py")
-run_command(f"{activation_command}python {file_to_execute}")
+# Étape 4 : Exécuter directement le script principal
+if os.path.exists(file_to_execute):
+    print(f"\nApplication lancée\n")
+    run_command(f"{activation_command}python {file_to_execute}")
+    
+else:
+    print(f"Le fichier '{file_to_execute}' est introuvable.")
